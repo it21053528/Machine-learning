b2ba36483788b91b7cb414cb0c5f7e39e7079b8b
surith <surith1234@gmail.com>
2022-12-06 07:51:35 +0530

    tensorflow added

---
+++ Test/test.py
@@ -0,0 +1,14 @@
+class Dog:
+    def __init__(self,Name,age):
+        self.name = Name
+        self.age = age
+
+
+d1 = Dog("tom",34)
+
+#print("Dog's name is ",d1.age)
+
+from math import sin
+
+x = "Hello"[1]
+print(list(reversed(range(1,11))))
